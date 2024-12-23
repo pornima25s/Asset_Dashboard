f842fb050f50b124e8ea64267ee5deacb940c902
pornima25s <sadanshivsudhir794@gmail.com>
2024-12-23 22:26:26 +0530

    Add SideNav and pages

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from "react";
+import Sidenav from "./Sidenav";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Setting from "./pages/Setting";
+
+export default function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" exact element={<Home />} />
+          <Route path="/about" exact element={<About />} />
+          <Route path="/setting" exact element={<Setting />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+}
